94dbc6ab0bf02d8bf1836354f19d6876fb9e6018
developer <sonimonesh115@gmail.com>
2023-10-03 16:30:04 +0530

    add the read testcases

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  before(:all) do
-    @product = Product.create(name: "Rspec testcases")
+  before(:each) do
+    @product = Product.create(name:"Rspec testcases")
+    expect(Product.find_by_name("Rspec testcases")).to eq(@product)
   end
 
   it 'checks that a Product  can be created' do
     expect(@product).to be_valid
+    
   end
+
+  it 'checks that a product can be read' do
+    expect(Product.find_by_name("Rspec testcases")).to eq(@product)
+  end
+
 end
