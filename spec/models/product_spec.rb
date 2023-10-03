7f943e80bc96879e0d7b43172c989e87802b7506
developer <sonimonesh115@gmail.com>
2023-10-03 16:36:18 +0530

    add the update testcases

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   before(:each) do
     @product = Product.create(name:"Rspec testcases")
-    expect(Product.find_by_name("Rspec testcases")).to eq(@product)
+    # expect(Product.find_by_name("Rspec testcases")).to eq(@product)
   end
 
   it 'checks that a Product  can be created' do
@@ -13,6 +13,11 @@ RSpec.describe Product, type: :model do
 
   it 'checks that a product can be read' do
     expect(Product.find_by_name("Rspec testcases")).to eq(@product)
+  end
+
+  it 'checks that a Product can be updated' do
+    @product.update(:name => "updated Rspec testcases")
+    expect(Product.find_by_name("updated Rspec testcases")).to eq(@product)
   end
 
 end
